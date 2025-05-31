bba6d136b434ffa1bc4d65c5c82aec5268cf3932
ilhamust <ilhammstqm8@gmail.com>
2025-05-31 22:50:22 +0700

    fixAppDuplicated

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style.css'
-import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
