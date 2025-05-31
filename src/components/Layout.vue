021beaca536f4736d4d4f9c58d86ec86fa4ca807
ilhamust <ilhammstqm8@gmail.com>
2025-05-31 22:39:35 +0700

    addLayout

---
+++ src/components/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header class="p-4 bg-green-700 text-white">
+      <h1 class="text-2xl font-bold">GAMA</h1>
+    </header>
+    <main class="p-4">
+      <slot />
+    </main>
+    <footer class="bg-gray-200 p-4 text-center text-sm">
+      &copy; 2025 GAMA. Semua Hak Dilindungi.
+    </footer>
+  </div>
+</template>
